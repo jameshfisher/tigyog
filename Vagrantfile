dccb9ff6ee74e6951e16085ef6dabc1cdf7a3629
James Fisher <jameshfisher@gmail.com>
2014-08-03 23:23:29 +0100

    hello world web server

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,7 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.provision "puppet"
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+  end
 end
--- src/Tigyog/Web.hs
+++ src/Tigyog/Web.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, world!"
+import Web.Scotty
+
+import Data.Monoid (mconcat)
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    word <- param "word"
+    html $ mconcat [word]
