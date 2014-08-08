790867075704518ee0e43ba2951d28485eb8f001
James Fisher <jameshfisher@gmail.com>
2014-08-08 19:38:45 +0000

    proof of concept using gitlib

--- src/Tigyog/Web.hs
+++ src/Tigyog/Web.hs
@@ -3,9 +3,14 @@ module Main where
 
 import Web.Scotty
 
+import Tigyog.Git (getCommitMessage)
+
 import Data.Monoid (mconcat)
+import Control.Monad.IO.Class (liftIO)
+import Data.Text.Lazy (fromStrict)
 
 main = scotty 3000 $ do
-  get "/:word" $ do
-    word <- param "word"
-    html $ mconcat [word]
+  get "/api/v1/commit/:hash/message" $ do
+    hash <- param "hash"
+    author <- liftIO $ getCommitMessage hash
+    html $ fromStrict author
