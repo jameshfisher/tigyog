d1463941843a332689bb94c4812e94a8fdcb082b
James Fisher <jameshfisher@gmail.com>
2014-08-10 23:24:25 +0000

    get file on commit

--- src/Tigyog/Git.hs
+++ src/Tigyog/Git.hs
@@ -1,8 +1,10 @@
-module Tigyog.Git (getCommitMessage, getBlobContents) where
+module Tigyog.Git (getCommitMessage, getBlobContents, getFileContents) where
 
-import Git hiding (CommitOid, Commit, BlobOid)
+import Git hiding (CommitOid, Commit, BlobOid, Tree, TreeOid, TreeEntry)
 import Git.Libgit2
+import Git.Libgit2.Types
 import Data.Text
+import Data.Text.Encoding (encodeUtf8)
 import Control.Monad.Reader
 import Control.Monad.Logger
 import Data.Tagged
@@ -54,3 +56,30 @@ getBlobContents :: Text -> IO Text
 getBlobContents h = do
   oid <- parseOid' h
   catBlobUtf8' $ toBlobOid oid
+
+lookupTree' :: TreeOid -> IO Tree
+lookupTree' = runStack . lookupTree
+
+treeEntry' :: Tree -> TreeFilePath -> IO (Maybe TreeEntry)
+treeEntry' t tfp = runStack $ treeEntry t tfp
+
+-- path looks like "foo/bar"; no leading slash
+commitAndPathToBlobOid :: Text -> Text -> IO (Maybe BlobOid)
+commitAndPathToBlobOid commitHash path = do
+  commit <- getCommit commitHash
+  let tOid = commitTree commit
+  tree <- lookupTree' tOid
+  te <- treeEntry' tree (encodeUtf8 path)
+  case te of
+    Just (BlobEntry bOid _) -> return $ Just bOid
+    Just _                  -> return Nothing
+    Nothing                 -> return Nothing
+
+getFileContents :: Text -> Text -> IO (Maybe Text)
+getFileContents commit path = do
+  bOid <- commitAndPathToBlobOid commit path
+  case bOid of
+    Just b -> do
+      t <- catBlobUtf8' b
+      return $ Just t
+    Nothing -> return Nothing
--- src/Tigyog/Web.hs
+++ src/Tigyog/Web.hs
@@ -2,8 +2,9 @@
 module Main where
 
 import Web.Scotty
+import Network.HTTP.Types.Status (status404)
 
-import Tigyog.Git (getCommitMessage, getBlobContents)
+import Tigyog.Git (getCommitMessage, getBlobContents, getFileContents)
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Text.Lazy (fromStrict)
@@ -15,7 +16,19 @@ main = scotty 3000 $ do
     author <- liftIO $ getCommitMessage hash
     html $ fromStrict author
 
+  -- deprecated.
+  -- API should not expose blobs directly.
+  -- This makes access control difficult to reason about.
+  -- File access is purely by branch+path, below.
   get "/api/v1/blob/:hash" $ do
     hash <- param "hash"
     blobText <- liftIO $ getBlobContents hash
     text $ fromStrict blobText
+
+  get (regex "^/api/v1/file/([^/]*)/(.*)$") $ do
+    commit <- param "1" -- URL-encoded
+    path <- param "2"
+    mBlobText <- liftIO $ getFileContents commit path
+    case mBlobText of
+      Just blobText -> text $ fromStrict blobText
+      Nothing -> status status404
